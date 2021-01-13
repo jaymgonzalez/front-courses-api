aabb1f3dfa335514dc05899436c51e39515c26f0
Jaime Munoz <jaimungon@gmail.com>
2021-01-13 14:22:13 +0100

    [ADD] gh pages and config

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { useUser } from '../user/userContext'
 import CourseComponent from './CourseComponent'
 import Dropdown from './common/Dropdown'
